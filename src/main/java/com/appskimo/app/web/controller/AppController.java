2206c1adbd6a70bfd1e2bf5df90a46e2bda3bc7d
dominic.lee <dominic.lee@kakaocorp.com>
2016-10-13 17:48:58 +0900

    no message

--- src/main/java/com/appskimo/app/web/controller/AppController.java
+++ src/main/java/com/appskimo/app/web/controller/AppController.java
@@ -1,28 +1,26 @@
 package com.appskimo.app.web.controller;
 
-import com.appskimo.app.domain.Mail;
-import com.appskimo.app.service.MailService;
+import com.google.common.collect.Maps;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
+import java.util.Map;
 
 /**
  * Created by dominic.lee on 2016. 10. 13..
  */
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class MailController {
+public class AppController {
 
-    @Inject
-    private MailService mailService;
+    @RequestMapping(value = "sample", method = RequestMethod.GET)
+    public Object sample() {
+        Map<String, String> map = Maps.newHashMap();
+        map.put("data", "sample");
 
-    @RequestMapping(value = "mail/send", method = RequestMethod.POST)
-    public void send(@RequestBody Mail mail) {
-        mailService.asyncSend(mail);
+        return map;
     }
-
+    
 }
